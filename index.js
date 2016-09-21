4a7b34af2120fc716ef012e3f535c973a3d7307c
jmas <jmas.ukraine@gmail.com>
2016-09-22 00:59:31 +0300

    Update refresh data behavior

--- index.js
+++ index.js
@@ -1,7 +1,9 @@
-import { el, apply } from 'el_';
+import { el, apply, refresh } from 'el_';
 import { createStore } from 'redux';
 import * as actions from './actions';
 import reducer from './reducer';
+import diff from 'deep-diff';
+import clone from 'clone';
 
 const store = createStore(reducer);
 
@@ -10,20 +12,23 @@ const FILTER_DONE = 'done';
 const FILTER_UNDONE = 'undone';
 
 function renderFlter () {
-  return apply(el(`
+  return apply(`
     <ul>
       <li><a data-all-btn href="#${FILTER_ALL}">All</a></li>
       <li><a data-done-btn href="#${FILTER_DONE}">Done</a></li>
       <li><a data-undone-btn href="#${FILTER_UNDONE}">Undone</a></li>
     </ul>
-  `), {
+  `, {
     '[data-all-btn]': (el) => el.onclick = (event) => {
+      event.preventDefault();
       store.dispatch(actions.filter(FILTER_ALL));
     },
     '[data-done-btn]': (el) => el.onclick = (event) => {
+      event.preventDefault();
       store.dispatch(actions.filter(FILTER_DONE));
     },
     '[data-undone-btn]': (el) => el.onclick = (event) => {
+      event.preventDefault();
       store.dispatch(actions.filter(FILTER_UNDONE));
     }
   });
@@ -54,6 +59,9 @@ function renderTask (task, index) {
 }
 
 function renderTasks (tasks, filter) {
+  if (tasks.length===0) {
+    return el(`<p>Empty.</p>`);
+  }
   return el(tasks.map((task, index) => {
     switch (filter) {
       case FILTER_DONE:
@@ -72,12 +80,12 @@ function renderTasks (tasks, filter) {
 }
 
 function renderForm () {
-  return apply(el(`
+  return apply(`
     <form data-form>
       <input data-query type="text" name="query" />
       <input type="submit" />
     </form>
-  `), {
+  `, {
     '[data-form]': (el) => el.onsubmit = (event) => {
       event.preventDefault();
       let queryEl = el.querySelector('input[name="query"]');
@@ -86,28 +94,46 @@ function renderForm () {
         done: false
       }));
       queryEl.value = '';
-    }
-  });
-}
-
-function renderApp (rootEl, data) {
-  return apply(rootEl, {
-    '#app': el(`
-      <div data-form></div>
-      <nav data-filter></nav>
-      <ul data-tasks></ul>
-    `),
-    '[data-filter]': renderFlter(data.filter),
-    '[data-tasks]':  renderTasks(data.tasks, data.filter),
-    '[data-form]':   renderForm(),
-    '[data-form] [data-query]': (el) => {
+      queryEl.focus();
+    },
+    '[data-query]': (el) => {
       el.focus();
     }
   });
 }
 
+function link (fn, paramNames, store) {
+  let state = clone(store.getState());
+  let el = fn.apply(null, paramNames.map((paramName) => state[paramName]));
+  store.subscribe(() => {
+    let newState = store.getState();
+    let diffs = diff(state, newState);
+    if (!diffs) {
+      return;
+    }
+    if (diffs.some((d) => paramNames.indexOf(d.path[0])!==-1)) {
+      refresh(el, fn.apply(null, paramNames.map((paramName) => newState[paramName])));
+    }
+    state = clone(newState);
+  });
+  return el;
+}
+
+function renderApp (rootEl, data) {
+  return apply(rootEl, {
+    '#app': `
+      <div data-form></div>
+      <nav data-filter></nav>
+      <ul data-tasks></ul>
+    `,
+    '[data-filter]': link(renderFlter, ['filter'], store),
+    '[data-tasks]':  link(renderTasks, ['tasks', 'filter'], store),
+    '[data-form]':   renderForm()
+  });
+}
+
 function main () {
-  store.subscribe(() => renderApp(document.body, store.getState()));
+  renderApp(document.body, store.getState());
   store.dispatch(actions.init({
     filter: location.hash.substring(1) || FILTER_ALL,
     tasks: [
